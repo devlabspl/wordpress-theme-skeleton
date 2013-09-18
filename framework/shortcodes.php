2d58cc5dce8bd3df2bec8726192a52c9a7d169d5
Marcin Szepczyński <szepczynski@gmail.com>
2013-09-18 15:56:15 +0200

    Initial commit

---
+++ framework/shortcodes.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+// [bartag foo="foo-value"]
+function bartag_func( $atts ) {
+	extract( shortcode_atts( array(
+		'foo' => 'something',
+		'bar' => 'something else',
+	), $atts ) );
+
+	return "foo = {$foo}";
+}
+add_shortcode( 'bartag', 'bartag_func' );
+*/
+
+?>
