6cecf608cf556913ad09bc0ca43631c82101035a
Marcin Szepczyński <szepczynski@gmail.com>
2013-09-18 16:30:58 +0200

    Custom taxonomy skelet

---
+++ framework/custom-taxonomies.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+function custom_taxonomy() {
+	register_taxonomy(
+		'taxonomy_name',
+		'post_type',
+		array(
+			'label' => 'Taxonomy label',
+			'rewrite' => array( 'slug' => 'tax-slug' ),
+			'hierarchical' => true,
+		)
+	);
+}
+add_action( 'init', 'custom_taxonomy' );
+*/
