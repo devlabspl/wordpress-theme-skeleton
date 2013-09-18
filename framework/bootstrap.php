fe86bf657dae8090a901816911d5ceebbb1e3bd5
Marcin Szepczyński <szepczynski@gmail.com>
2013-09-18 16:30:36 +0200

    Typo error

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -4,7 +4,7 @@ require_once dirname( __FILE__ ) . '/custom-post-types.php';
 require_once dirname( __FILE__ ) . '/custom-taxonomies.php';
 require_once dirname( __FILE__ ) . '/shortcodes.php';
 
-add_theme_support( 'post-thumbnail' );
+add_theme_support( 'post-thumbnails' );
 add_theme_support( 'html5' );
 
 ?>
