042562741653a69f58d870b698282bfc61e153d3
Marcin Szepczyński <szepczynski@gmail.com>
2013-09-18 16:36:11 +0200

    Sidebar skelet

---
+++ framework/sidebars.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+register_sidebar(array(
+	'name'          => __( 'Sidebar name', 'theme_text_domain' ),
+	'id'            => 'unique-sidebar-id',
+	'description'   => '',
+    'class'         => '',
+	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+	'after_widget'  => '</li>',
+	'before_title'  => '<h2 class="widgettitle">',
+	'after_title'   => '</h2>' 
+));
+*/
